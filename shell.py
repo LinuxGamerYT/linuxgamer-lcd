ba528b946b0465eb1e3e91799044b31be05c3320
LinuxGamerYT <116877626+LinuxGamerYT@users.noreply.github.com>
2023-06-30 19:45:20 +0200

    Update shell.py

--- shell.py
+++ shell.py
@@ -2,6 +2,7 @@ from os import system
 
 cmd = input("$ ")
 if cmd == 'install':
+    system("sudo chmod +x install.sh")
     system("./install.sh")
 if cmd == 'rotate':
     system("python3 rotate.py")
